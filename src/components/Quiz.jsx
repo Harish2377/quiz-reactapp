63999008a512bc6915a709ba57fd365e393381b1
Harish2377 <harishvmworks777@gmail.com>
2023-11-04 20:29:02 +0530

    quiz-app-v1

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -13,7 +13,7 @@ const Quiz = () => {
                         <div className="card p-4 bg-black  " style={{ background: '#3d3d3d', borderColor: 'red' }}>
                             <div className="d-flex justify-content-between gap-md-3">
                                 <h5 className='mb-2 fs-normal lh-base'>{question?.question}</h5>
-                                <h5 style={{ color: 'red', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
+                               <div> <h5 style={{ color: 'red', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5></div>
                             </div>
                             <div>
                                 {
